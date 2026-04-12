dc6799ce2f81771773699e229cae57ab4b81f04b
Mohamed Hazem <mohamedsaleh2233445@gmail.com>
2026-04-12 16:06:48 +0200

    Delete Deploy directory

--- Deploy/detector/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PredictAPIView, IndexView, health
-
-urlpatterns = [
-    path('',               IndexView.as_view(),    name='index'),
-    path('api/predict/',   PredictAPIView.as_view(), name='predict'),
-    path('health/',        health,                  name='health'),
-]
