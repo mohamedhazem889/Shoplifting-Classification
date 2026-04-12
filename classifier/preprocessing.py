b8ab023a08f0bd2e4b44214d1e8908ccbf956cc9
Mohamed Hazem <mohamedsaleh2233445@gmail.com>
2026-04-12 12:43:15 +0200

    Delete classifier directory

--- classifier/preprocessing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def preprocess_data(df):
-    # Implement preprocessing steps replicating notebook logic
-    # Example: Clean columns, fill missing values, etc.
-    df.fillna(method='ffill', inplace=True)
-    return df
